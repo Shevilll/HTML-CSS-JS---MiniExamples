f029feff8bf59d854a1e3642bd8d94c5965b2677
Shevilll <ahmadfaraz00710@gmail.com>
2023-04-26 16:16:57 +0530

    extras

---
+++ add-remove html/main.js
@@ -0,0 +1,10 @@
+let name = document.createElement("h1")
+name.id="hello"
+name.textContent = "Hello, World"
+document.body.append(name)
+
+document.body.append(document.createElement('hr'))
+
+document.body.append(document.createElement('label').textContent = "HII")
+
+document.getElementById("hello").style.backgroundColor="yellow"
