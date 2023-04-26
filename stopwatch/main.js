d82bb699529099685c881e7cb4befaa3a9c8d3b0
Shevilll <ahmadfaraz00710@gmail.com>
2023-04-26 13:58:12 +0530

    added miliseconds

--- stopwatch/main.js
+++ stopwatch/main.js
@@ -7,12 +7,13 @@ let elapsedtime=0
 let secs=0
 let mins=0
 let hrs=0
+let ms=0
 
 document.getElementById("start").onclick = function(){
     if (paused){
         paused=false
         startime = Date.now() - elapsedtime
-        timer = setInterval(update,1000)
+        timer = setInterval(update,100)
     }
 }
 document.getElementById("stop").onclick = function(){
@@ -22,7 +23,7 @@ document.getElementById("stop").onclick = function(){
     }
 }
 document.getElementById("reset").onclick = function(){
-    time.textContent = `00:00:00`
+    time.textContent = `00:00:00:00`
     paused = true
     clearInterval(timer)
     elapsedtime = 0
@@ -34,15 +35,20 @@ function update(){
     secs = Math.floor((elapsedtime/1000)%60)
     mins = Math.floor((elapsedtime/60000)%60)
     hrs = Math.floor((elapsedtime/3600000)%60)
-
+    ms++
+    if (ms==100){
+        ms=0
+    }
     secs = padding(secs)
     mins = padding(mins)
     hrs = padding(hrs)
+    ms = padding(ms)
 
-    time.textContent = `${hrs}:${mins}:${secs}`
+    time.textContent = `${hrs}:${mins}:${secs}:${ms}`
     function padding(text){
         if (text < 10){
             return `0${text}`
         }
+        return text
     }
 }
